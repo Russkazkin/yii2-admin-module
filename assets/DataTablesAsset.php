70504e7a1b12bb96c615d3975fbf093617cb89f8
skazkin <russkazkin@gmail.com>
2019-12-14 16:37:05 +0300

    Add js files to DataTablesAsset

--- assets/DataTablesAsset.php
+++ assets/DataTablesAsset.php
@@ -8,5 +8,32 @@ use yii\web\AssetBundle;
 
 class DataTablesAsset extends AssetBundle
 {
+    public $sourcePath = '@app/modules/admin/resources';
 
+    public $css = [
+        'css/app.min.css',
+        'css/bootstrap.min.css',
+        'css/icons.min.css',
+    ];
+
+    public $js = [
+        'js/libs/datatables/jquery.dataTables.min.js',
+        'js/libs/datatables/dataTables.bootstrap4.js',
+        'js/libs/datatables/dataTables.responsive.min.js',
+        'js/libs/datatables/responsive.bootstrap4.min.js',
+        'js/libs/datatables/dataTables.buttons.min.js',
+        'js/libs/datatables/buttons.bootstrap4.min.js',
+        'js/libs/datatables/buttons.html5.min.js',
+        'js/libs/datatables/buttons.flash.min.js',
+        'js/libs/datatables/buttons.print.min.js',
+        'js/libs/datatables/dataTables.keyTable.min.js',
+        'js/libs/datatables/dataTables.select.min.js',
+        'js/libs/pdfmake/pdfmake.min.js',
+        'js/libs/pdfmake/vfs_fonts.js',
+        'js/pages/datatables.init.js'
+    ];
+
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
 }
