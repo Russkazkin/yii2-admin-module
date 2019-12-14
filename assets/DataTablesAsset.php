4cadf376b7dfcaf984601646dfa231437884761a
skazkin <russkazkin@gmail.com>
2019-12-14 16:48:28 +0300

    Add css to DataTablesAsset

--- assets/DataTablesAsset.php
+++ assets/DataTablesAsset.php
@@ -11,25 +11,26 @@ class DataTablesAsset extends AssetBundle
     public $sourcePath = '@app/modules/admin/resources';
 
     public $css = [
-        'css/app.min.css',
-        'css/bootstrap.min.css',
-        'css/icons.min.css',
+        'libs/datatables/dataTables.bootstrap4.css',
+        'libs/datatables/responsive.bootstrap4.css',
+        'libs/datatables/buttons.bootstrap4.css',
+        'libs/datatables/select.bootstrap4.css',
     ];
 
     public $js = [
-        'js/libs/datatables/jquery.dataTables.min.js',
-        'js/libs/datatables/dataTables.bootstrap4.js',
-        'js/libs/datatables/dataTables.responsive.min.js',
-        'js/libs/datatables/responsive.bootstrap4.min.js',
-        'js/libs/datatables/dataTables.buttons.min.js',
-        'js/libs/datatables/buttons.bootstrap4.min.js',
-        'js/libs/datatables/buttons.html5.min.js',
-        'js/libs/datatables/buttons.flash.min.js',
-        'js/libs/datatables/buttons.print.min.js',
-        'js/libs/datatables/dataTables.keyTable.min.js',
-        'js/libs/datatables/dataTables.select.min.js',
-        'js/libs/pdfmake/pdfmake.min.js',
-        'js/libs/pdfmake/vfs_fonts.js',
+        'libs/datatables/jquery.dataTables.min.js',
+        'libs/datatables/dataTables.bootstrap4.js',
+        'libs/datatables/dataTables.responsive.min.js',
+        'libs/datatables/responsive.bootstrap4.min.js',
+        'libs/datatables/dataTables.buttons.min.js',
+        'libs/datatables/buttons.bootstrap4.min.js',
+        'libs/datatables/buttons.html5.min.js',
+        'libs/datatables/buttons.flash.min.js',
+        'libs/datatables/buttons.print.min.js',
+        'libs/datatables/dataTables.keyTable.min.js',
+        'libs/datatables/dataTables.select.min.js',
+        'libs/pdfmake/pdfmake.min.js',
+        'libs/pdfmake/vfs_fonts.js',
         'js/pages/datatables.init.js'
     ];
 
