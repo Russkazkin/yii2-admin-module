5eb9fef9a726c4782779a8fc761de4c1c599a095
skazkin <russkazkin@gmail.com>
2019-12-17 14:01:06 +0300

    Add base controller with rbac functionality

---
+++ controllers/BaseController.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace app\modules\admin\controllers;
+
+
+use Yii;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+use yii\web\Controller;
+
+class BaseController extends Controller
+{
+    public $rbacManager;
+
+    public function __construct($id, $module, $config = [])
+    {
+        $this->rbacManager = Yii::$app->getModule('auth')->rbac;
+        $this->view->params['adminPermissions'] = $this->rbacManager->haveAdminPermissions();
+
+        parent::__construct($id, $module, $config);
+    }
+
+    public function behaviors()
+    {
+        return array_merge(
+            parent::behaviors(),
+            [
+                'verbs' => [
+                    'class' => VerbFilter::class,
+                    'actions' => [
+                        'delete' => ['POST'],
+                    ],
+                ],
+                'access' => [
+                    'class' => AccessControl::class,
+                    'rules' => [
+                        [
+                            'allow' => true,
+                            'roles' => ['@'],
+                        ],
+                    ],
+                ],
+            ]);
+    }
+}
