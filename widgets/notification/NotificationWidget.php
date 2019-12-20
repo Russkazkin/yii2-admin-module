485c90ededb68dfcfa8c6e48a8af65e4984f60dd
skazkin <russkazkin@gmail.com>
2019-12-20 15:07:11 +0300

    Add test notification output

--- widgets/notification/NotificationWidget.php
+++ widgets/notification/NotificationWidget.php
@@ -5,6 +5,7 @@ namespace app\modules\admin\widgets\notification;
 
 
 use app\modules\admin\widgets\notification\assets\NotificationWidgetAsset;
+use Yii;
 use yii\base\Widget;
 
 class NotificationWidget extends Widget
@@ -17,6 +18,26 @@ class NotificationWidget extends Widget
 
     public function run()
     {
-        return '<h1>it works!</h1>';
+        $this->view->registerJs("
+            Command: toastr[\"success\"](\"My name is Inigo Montoya. You killed my father. Prepare to die!\")
+
+toastr.options = {
+  \"closeButton\": false,
+  \"debug\": false,
+  \"newestOnTop\": false,
+  \"progressBar\": false,
+  \"positionClass\": \"toast-top-right\",
+  \"preventDuplicates\": false,
+  \"onclick\": null,
+  \"showDuration\": \"300\",
+  \"hideDuration\": \"1000\",
+  \"timeOut\": \"5000\",
+  \"extendedTimeOut\": \"1000\",
+  \"showEasing\": \"swing\",
+  \"hideEasing\": \"linear\",
+  \"showMethod\": \"fadeIn\",
+  \"hideMethod\": \"fadeOut\"
+}
+        ");
     }
 }
--- widgets/notification/assets/NotificationWidgetAsset.php
+++ widgets/notification/assets/NotificationWidgetAsset.php
@@ -4,9 +4,22 @@
 namespace app\modules\admin\widgets\notification\assets;
 
 
+use app\modules\admin\assets\AdminAsset;
 use yii\web\AssetBundle;
 
 class NotificationWidgetAsset extends AssetBundle
 {
-    public $sourcePath = '@app/modules/admin/resources';
+    public $sourcePath = '@app/modules/admin/widgets/notification/resources';
+
+    public $css = [
+        'css/toastr.min.css'
+    ];
+
+    public $js = [
+        'js/toastr.min.js'
+    ];
+
+    public $depends = [
+        AdminAsset::class,
+    ];
 }
