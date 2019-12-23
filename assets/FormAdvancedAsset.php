4db3c4ae24fb718697038e877b0a8f8a3cb0166e
skazkin <russkazkin@gmail.com>
2019-12-23 20:02:11 +0300

    Add multiselect plagin

--- assets/FormAdvancedAsset.php
+++ assets/FormAdvancedAsset.php
@@ -11,11 +11,13 @@ class FormAdvancedAsset extends AssetBundle
     public $sourcePath = '@app/modules/admin/resources';
 
     public $css = [
-        'libs/select2.min.css'
+        'libs/select2/select2.min.css',
+        'libs/multiselect/multi-select.css',
     ];
 
     public $js = [
-        'libs/select2.min.js'
+        'libs/select2/select2.min.js',
+        'libs/multiselect/jquery.multi-select.js',
     ];
 
     public $depends = [
--- resources/js/init.js
+++ resources/js/init.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
     $('#article-category_id').select2();
+    $('#article-tags').select2();
 });
