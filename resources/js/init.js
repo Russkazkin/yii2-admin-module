22814086a1024defef0b34b1dad39d161ddd2ed3
skazkin <russkazkin@gmail.com>
2019-12-24 14:49:41 +0300

    Add select placeholder

--- resources/js/init.js
+++ resources/js/init.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
-    $('#article-category_id').select2();
+    $('#article-category_id').select2(
+        {
+            placeholder: "Choose category...",
+            allowClear: true
+        }
+    );
     $('#article-tags').select2({
-        placeholder: "Select a state"
+        placeholder: "Choose tags..."
     });
 });
