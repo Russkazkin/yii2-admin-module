7ee5edfd53b2bba5ede6b1d047326ab239716981
skazkin <russkazkin@gmail.com>
2019-12-28 08:57:50 +0300

    Add maxlength badge to article title input

--- assets/FormAdvancedAsset.php
+++ assets/FormAdvancedAsset.php
@@ -11,13 +11,16 @@ class FormAdvancedAsset extends AssetBundle
     public $sourcePath = '@app/modules/admin/resources';
 
     public $css = [
+        'libs/bootstrap-tagsinput/bootstrap-tagsinput.css',
         'libs/select2/select2.min.css',
         'libs/multiselect/multi-select.css',
     ];
 
     public $js = [
+        'libs/bootstrap-tagsinput/bootstrap-tagsinput.min.js',
         'libs/select2/select2.min.js',
         'libs/multiselect/jquery.multi-select.js',
+        'libs/bootstrap-maxlength/bootstrap-maxlength.min.js',
         'js/init.js',
     ];
 
--- resources/js/init.js
+++ resources/js/init.js
@@ -8,4 +8,14 @@ $(document).ready(function() {
     $('#article-tags').select2({
         placeholder: "Choose tags..."
     });
+
+    $('.article-form #article-title').maxlength({
+        threshold: 64,
+        warningClass: "badge badge-success",
+        limitReachedClass: "badge badge-danger",
+        validate: true,
+        alwaysShow: true,
+        placement: 'top-left'
+    });
+
 });
