7bcb0a7bf9f6f7197f093a92e2d645ae808e80d9
skazkin <russkazkin@gmail.com>
2019-12-28 16:45:23 +0300

    Add quill editor widget

--- assets/FormAdvancedAsset.php
+++ assets/FormAdvancedAsset.php
@@ -14,6 +14,9 @@ class FormAdvancedAsset extends AssetBundle
         'libs/bootstrap-tagsinput/bootstrap-tagsinput.css',
         'libs/select2/select2.min.css',
         'libs/multiselect/multi-select.css',
+        'libs/quill/quill.core.css',
+        'libs/quill/quill.bubble.css',
+        'libs/quill/quill.snow.css',
     ];
 
     public $js = [
@@ -21,6 +24,8 @@ class FormAdvancedAsset extends AssetBundle
         'libs/select2/select2.min.js',
         'libs/multiselect/jquery.multi-select.js',
         'libs/bootstrap-maxlength/bootstrap-maxlength.min.js',
+        'libs/katex/katex.min.js',
+        'libs/quill/quill.min.js',
         'js/init.js',
     ];
 
--- resources/js/init.js
+++ resources/js/init.js
@@ -26,6 +26,13 @@ $(document).ready(function() {
         alwaysShow: true,
         placement: 'top-left'
     });
+
+
+    /*new Quill("#artice-content", {
+        theme: "snow",
+        modules: {toolbar: [[{font: []}, {size: []}], ["bold", "italic", "underline", "strike"], [{color: []}, {background: []}], [{script: "super"}, {script: "sub"}], [{header: [!1, 1, 2, 3, 4, 5, 6]}, "blockquote", "code-block"], [{list: "ordered"}, {list: "bullet"}, {indent: "-1"}, {indent: "+1"}], ["direction", {align: []}], ["link", "image", "video", "formula"], ["clean"]]}
+    });*/
+
 });
 
 /*var quill = new Quill('#editor-container', {
