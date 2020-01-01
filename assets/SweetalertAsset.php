af2d7bbb59f91b2d88d5d567728aa7d30a5c3fda
Ruslan Skazkopodatelev <ruslan@skazkin.su>
2020-01-01 11:11:24 +0300

    Add SweetalertAsset bundle

---
+++ assets/SweetalertAsset.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace app\modules\admin\assets;
+
+
+use yii\web\AssetBundle;
+
+class SweetalertAsset extends AssetBundle
+{
+    public $sourcePath = '@app/modules/admin/resources';
+
+    public $css = [
+        'libs/sweetalert2/sweetalert2.min.css'
+    ];
+
+    public $js = [
+        'libs/sweetalert2/sweetalert2.min.js'
+    ];
+}
