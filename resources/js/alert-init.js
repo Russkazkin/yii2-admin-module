b41a1201fe1a9c762cb6c5cf028aec5f33ac40e3
skazkin <russkazkin@gmail.com>
2020-01-06 10:47:34 +0300

    Add MVP Sweetalert delete category

--- resources/js/alert-init.js
+++ resources/js/alert-init.js
@@ -1,26 +1,57 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $(".article-list-delete").on("click", function (event) {
         event.preventDefault();
+        let id = $(this).data('id');
         Swal.fire({
             title: "Are you sure?",
             text: "You won't be able to revert this!",
             type: "warning",
-            showCancelButton: !0,
-            confirmButtonText: "Yes, delete it!",
-            cancelButtonText: "No, cancel!",
+            showCancelButton: true,
+            confirmButtonText: "Delete",
+            cancelButtonText: "Cancel",
             confirmButtonClass: "btn btn-success mt-2",
             cancelButtonClass: "btn btn-danger ml-2 mt-2",
-            buttonsStyling: !1
-        }).then(function (t) {
-            t.value ? Swal.fire({
-                title: "Deleted!",
-                text: "Your file has been deleted.",
-                type: "success"
-            }) : t.dismiss === Swal.DismissReason.cancel && Swal.fire({
-                title: "Cancelled",
-                text: "Your imaginary file is safe :)",
-                type: "error"
-            });
+            buttonsStyling: !1,
+            preConfirm: () => {
+                return fetch(`/admin/blog/category/delete?id=${id}`, {
+                    method: 'POST',
+                    headers : {
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json'
+                    }
+                })
+                    .then(response => {
+                    if (!response.ok) {
+                        throw new Error(response.statusText)
+                    }
+                    return response;
+                })
+                    .catch(error => {
+                        Swal.showValidationMessage(
+                            `Request failed: ${error}`
+                        )
+                    })
+            },
+            allowOutsideClick: () => !Swal.isLoading()
+        }).then((result) => {
+                console.log(result.value);
+                if(!result.value) {
+                    return false;
+                } else if (result.value.ok) {
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: "Your file has been deleted.",
+                        type: "success"
+                    });
+                    $(this).parent().parent().remove();
+                } else {
+                    Swal.fire({
+                        title: "Something went wrong",
+                        text: "We are very sorry!",
+                        type: "error"
+
+                });
+            }
         });
         return false;
     });
